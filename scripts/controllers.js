3449584d0762dd2ab9109cd7646a82c475f3af20
viabledish <viable.dish@gmail.com>
2013-11-09 05:24:16 +0000

    added drag and drop

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -1,28 +1,37 @@
-var todoApp = angular.module('todoApp', []);
+var todoApp = angular.module('todoApp', ['ui.sortable']);
 
 todoApp.controller('ItemCtrl', function ItemCtrl($scope) {
 	var id_counter = 1;
-
+  
 	$scope.items = [
-	{item: 'Item one'},
-	{item: 'Item two'},
-	{item: 'Item three'}
+		{item: 'Item one', complete: false},
+		{item: 'Item two', complete: false},
+		{item: 'Item three', complete: false}
 	];
 
 	$scope.addItem = function () {
-		debugger;
 	    id_counter += 1;
 	    $scope.items.unshift({item:$scope.newItem, sort_id:id_counter});
-	    $scope.newItem = null;
+	    $scope.newItem = "";
   	};
 
   	$scope.removeItem = function ( idx ) {
 	    $scope.items.splice( idx, 1 );
   	};
 
-  	$scope.change = function() {
-
+  	$scope.disableEditor = function( item ) {
+  		item.editorEnabled = false;
   	}
+
+  	$scope.enableEditor = function ( item ) {
+  		console.log(item);
+        item.editorEnabled = true;
+    };
+
+    $scope.toggleCompleteItem = function ( item ) {
+    	item.complete = !item.complete;
+    };
 
 });
 
+//angular.bootstrap(document, ['todoApp']);
